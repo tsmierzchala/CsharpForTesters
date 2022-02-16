cd901117a7150e9cdc5230ca985fb3b24459f9df
tsmierzchala <tomasz.smierzchala@forcom.com.pl>
2022-02-16 10:29:53 +0100

    zadanie 9

--- CsharpForTesters/01-Wstep/TypyObiektowe.cs
+++ CsharpForTesters/01-Wstep/TypyObiektowe.cs
@@ -42,7 +42,43 @@ namespace CsharpForTesters._01_Wstep
                 Console.WriteLine($"Pomiar wynosi {value}");
             }
 
+            //var value2 = (0 < value < 100) ? "To nie jest akceptowalna wartość zmiennej value" : value;
+
             //zadanie 8
+            double input = 5.5;
+            string jakiePiwo = (input >= 4) ? "mocne" : "słabe";
+            Console.WriteLine(jakiePiwo);
+
+            //zadanie 9
+            int dayOfTheWeek = 1;
+
+            switch (dayOfTheWeek)
+            {
+                case 1:
+                    Console.WriteLine("Poniedziałek");
+                    break;
+                case 2:
+                    Console.WriteLine("Wtorek");
+                    break;
+                case 3:
+                    Console.WriteLine("Środa");
+                    break;
+                case 4:
+                    Console.WriteLine("Czwartek");
+                    break;
+                case 5:
+                    Console.WriteLine("Piątek");
+                    break;
+                case 6:
+                    Console.WriteLine("Sobota");
+                    break;
+                case 7:
+                    Console.WriteLine("Niedziela");
+                    break;
+                default:
+                    Console.WriteLine("Błędny dzień");
+                    break;
+            }
 
         }
     }
