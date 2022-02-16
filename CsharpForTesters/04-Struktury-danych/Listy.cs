0928c6482a0dbc33eec8f0c31f8534b80f0d8f90
tsmierzchala <tomasz.smierzchala@forcom.com.pl>
2022-02-16 14:01:08 +0100

    Listy

--- CsharpForTesters/04-Struktury-danych/Listy.cs
+++ CsharpForTesters/04-Struktury-danych/Listy.cs
@@ -8,6 +8,27 @@ namespace CsharpForTesters._04_Struktury_danych
     {
         public static void Main(string[] args)
         {
+            List<string> starWars = new List<string>();
+
+            starWars.Add("Han Solo");
+            starWars.Add("Luke Skywalker");
+            starWars.Add("Darth Vader");
+            starWars.Add("C-3PO");
+            starWars.Add("Leia Organa");
+            starWars.Add("Obi-Wan Kenobi");
+
+            Console.WriteLine($"Last element on list {starWars[^1]}");   
+            
+            starWars.Reverse();
+            Console.WriteLine("\nReverse:");
+            foreach (string s in starWars) { Console.WriteLine(s); }
+
+            Console.WriteLine("\nSortowanie:");
+            starWars.Sort();
+
+            foreach (string s in starWars) { Console.WriteLine(s); }
+
+            Console.WriteLine($"\nPierwszy element: {starWars[0]}");
 
         }
     }
