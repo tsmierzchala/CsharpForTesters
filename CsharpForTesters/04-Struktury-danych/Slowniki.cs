d350448bb5b21d48c3b37f71d9aa6436f46cfa8b
tsmierzchala <tomasz.smierzchala@forcom.com.pl>
2022-02-16 14:14:13 +0100

    Dictionary

--- CsharpForTesters/04-Struktury-danych/Slowniki.cs
+++ CsharpForTesters/04-Struktury-danych/Slowniki.cs
@@ -8,6 +8,48 @@ namespace CsharpForTesters._04_Struktury_danych
     {
         public static void Main(string[] args)
         {
+            Dictionary<string, string> browserSettings = new Dictionary<string, string>();
+            browserSettings.Add("browser", "chrome");
+            browserSettings.Add("headless", "false");
+            browserSettings.Add("res_width", "1920");
+            browserSettings.Add("res_height", "1080");
+
+            try
+            {
+                Console.WriteLine("For key = \"browser\", value = {0}.", browserSettings["browser"]);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Key = \"browser\" is not found.");
+            }
+
+            try
+            {
+                Console.WriteLine("For key = \"headless\", value = {0}.", browserSettings["headless"]);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Key = \"headless\" is not found.");
+            }
+
+            try
+            {
+                browserSettings.Remove("headless");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("For key = \"headless\", value = {0}.", browserSettings["headless"]);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Key = \"headless\" is not found.");
+            }
+
         }
     }
 }
