0f9ab1a79575d3862207ad1edfa685bdbb618524
tsmierzchala <tomasz.smierzchala@forcom.com.pl>
2022-02-16 12:06:42 +0100

    Debuggowanie

--- CsharpForTesters/02-Debugowanie/DebugowanieWarunkowe.cs
+++ CsharpForTesters/02-Debugowanie/DebugowanieWarunkowe.cs
@@ -8,22 +8,22 @@ namespace CsharpForTesters._02_Debugowanie
     {
         public static void Main(string[] args)
         {
-            var v = 0;
+            //var v = 0;
 
-            for (int i = 1000; i >= 0; i--)
-            {
-                v = i < 9999 ? 10 / i : 10 * i;
-            }
+            //for (int i = 1000; i > 0; i--)
+            //{
+            //    v = i < 9999 ? 10 / i : 10 * i;
+            //}
 
-            //int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
-            //double result = GetAvgFromArray(nums);
-            //Console.WriteLine(result); 
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+            double result = GetAvgFromArray(nums);
+            Console.WriteLine(result);
         }
 
         public static double GetAvgFromArray(int[] arr)
         {
             double result = 0;
-            for (int i =0; i<=arr.Length; i++)
+            for (int i =0; i<=arr.Length-1; i++)
             {
                 result += arr[i];
             }
