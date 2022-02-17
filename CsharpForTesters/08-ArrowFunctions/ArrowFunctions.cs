7e1b45d21c9657fb8aeaa02a3b23e273bd94ed0d
tsmierzchala <tomasz.smierzchala@forcom.com.pl>
2022-02-17 09:33:23 +0100

    Linq

---
+++ CsharpForTesters/08-ArrowFunctions/ArrowFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CsharpForTesters._08_ArrowFuncitons
+{
+    class ArrowFunctions
+    {
+        public static void Main()
+        {
+
+
+        }
+    }
+}
