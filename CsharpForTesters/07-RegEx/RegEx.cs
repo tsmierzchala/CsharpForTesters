1f7787ceec9a55384ca48546ae264a91c5144b92
tsmierzchala <tomasz.smierzchala@forcom.com.pl>
2022-02-17 09:14:17 +0100

    RegEx

---
+++ CsharpForTesters/07-RegEx/RegEx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CsharpForTesters._07_RegEx
+{
+    class Test
+    {
+        public static void Main()
+        {
+            string path = @"C:\Users\LaptopSzkoleniowy\Documents\kod\CsharpForTesters\text1.txt";
+
+            // Open the file to read from.
+            string[] readText = File.ReadAllLines(path);
+            foreach (string s in readText)
+            {
+                Console.WriteLine(s);
+
+                Regex re = new Regex(@"\d+(,\d{0,2})?(zł|\$|€)");
+                MatchCollection result = re.Matches(s);
+                foreach (Match r in result)
+                {
+                    Console.WriteLine($"* Kwoty: {r.Value}");
+                }
+
+                Regex re2 = new Regex(@"(?<=[w]\s)([^ -.]*)");
+                MatchCollection result2 = re2.Matches(s);
+                foreach (Match r in result2)
+                {
+                    Console.WriteLine($"* Kraje: {r.Value}");
+                }
+
+                Regex re3 = new Regex(@"^\p{L}+");
+                MatchCollection result3 = re3.Matches(s);
+                foreach (Match r in result3)
+                {
+                    Console.WriteLine($"* Przedmiot: {r.Value}");
+                }
+
+            }
+
+
+
+        }
+    }
+}
